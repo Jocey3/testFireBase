9de4330da974e6266e3373a307a5e4c7ca7ddf7c
Oleksandr Holynkov <golinkovsasha@gmail.com>
2021-11-25 19:10:30 +0200

    prepared all staff

--- app/src/main/java/dev/jocey/testfirebase/MainActivity.java
+++ app/src/main/java/dev/jocey/testfirebase/MainActivity.java
@@ -1,6 +1,7 @@
 package dev.jocey.testfirebase;
 
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,15 +9,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigServerException;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -28,16 +37,23 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private EditText email;
     private EditText pass;
+    private TextView eDtoken;
+    private View sq;
+
     private Button login;
     private Button changePic;
     private ProgressBar progressBar;
     private int numPic = 0;
+    private String token = "";
 
     private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    private FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
     private FirebaseAuth auth;
-    FirebaseUser user;
+    private FirebaseUser user;
     private List<String> list = new ArrayList<>();
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +63,67 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         checkUser();
         login.setOnClickListener(view -> logining());
-        changePic.setOnClickListener(view -> setPic());
+        changePic.setOnClickListener(view -> {
+            setPic();
+            forSq();
+        });
+        getToken();
+
+        rem();
+        config();
+        Log.d(LOG, "JSON " + json);
+        forSq();
+
+
+    }
+
+    private String json = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void forSq() {
+        updateRemConf();
+        String col = remoteConfig.getString("color");
+        Log.d(LOG, "color " + col);
+        switch (col) {
+            case "red":
+                sq.setVisibility(View.VISIBLE);
+                sq.setBackground(getDrawable(R.drawable.red_s));
+                break;
+            case "green":
+                sq.setVisibility(View.VISIBLE);
+                sq.setBackground(getDrawable(R.drawable.green_s));
+                break;
+            default:
+                sq.setVisibility(View.GONE);
+
+        }
+    }
+
+    public void updateRemConf() {
+        rem();
+    }
+
+    public void config() {
+
+        json = remoteConfig.getValue("countries").asString();
+    }
+
+    FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(12)
+            .build();
+
+
+    public void rem() {
+        remoteConfig.setConfigSettingsAsync(configSettings);
+        remoteConfig.fetchAndActivate().addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(LOG, "rem " + task.getResult().toString());
+            } else {
+
+                Log.d(LOG, "rem not get");
+            }
+        });
+
     }
 
     @Override
@@ -56,6 +132,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    public void getToken() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        token = task.getResult();
+                        Log.d(LOG, token);
+                        eDtoken.setText(token);
+                    } else {
+                        Log.d(LOG, "TOKEN " + String.valueOf(task.getException()));
+                    }
+                });
+    }
+
 
     public void checkUser() {
         user = auth.getCurrentUser();
@@ -129,8 +225,10 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         email = findViewById(R.id.ed_email);
         pass = findViewById(R.id.ed_pass);
+        eDtoken = findViewById(R.id.token);
         login = findViewById(R.id.btn_login);
         changePic = findViewById(R.id.btn_change_pic);
+        sq = findViewById(R.id.col_sq);
     }
 
 
